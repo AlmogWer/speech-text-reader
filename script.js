7649640f8b7bec0ca2c9e00b1451f53f2daf08c6
Almog Wertzberger <Almogish@gmail.com>
2021-08-29 15:16:23 +0300

    adding functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const textarea = document.getElementById("text");
 const readBtn = document.getElementById("read");
 const toggleBtn = document.getElementById("toggle");
 const closeBtn = document.getElementById("close");
+const textBox = document.getElementById("text-box");
 
 const data = [
   {
@@ -67,7 +68,65 @@ function createBox(item) {
   <img src="${image}" alt="${text}" />
   <p class="info">${text}</p>
   `;
-  //@todo - speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    //add active effect
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
 
   main.appendChild(box);
 }
+
+//Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+//Store voices
+let voices = [];
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voicesSelect.appendChild(option);
+  });
+}
+
+//Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+//Set voice
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+//Speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+//Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+//Toggle text box
+toggleBtn.addEventListener("click", () => textBox.classList.toggle("show"));
+
+//Close button
+closeBtn.addEventListener("click", () => textBox.classList.toggle("show"));
+
+//Change voice
+voicesSelect.addEventListener("change", setVoice);
+
+//Read text button
+readBtn.addEventListener("click", () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getVoices();
